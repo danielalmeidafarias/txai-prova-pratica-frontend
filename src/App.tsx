629ce4d9fa9550219bd35431d237cef7fba18d95
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2024-09-22 13:45:07 -0300

    feat: setting up some important libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Products from "./pages/Products"
+
 function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/Products" element={<Products />} />
+    </Routes>
   )
 }
 
